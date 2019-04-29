3421c271a55d4875285dc0a390af111be7a83d2d
KnightRider <omar.brown@comcast.net>
2019-04-29 17:06:54 -0400

    4th commit

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,11 @@
- 
 // requirements: import mongoose
 const mongoose = require('mongoose')
 
-
-// Connect to a local database called "the-donut-shop"
-mongoose.connect("mongodb://localhost/the-donut-store", {useNewUrlParser:true}).then(()=>{
-    console.log ("Connected to MongoDB")
-})
-// When it connects, then console.log "Connected to MongoDB"
-
+const dbConnection = process.env.MONGODB_URI || 'mongodb://localhost:27017/project-2'
+mongoose.connect(dbConnection, { useNewUrlParser: true})
+  .then(() => {
+    console.log("mongo is working");
+  })
 
 // export your mongoose connection
-module.exports = mongoose
+module.exports = mongoose;
--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-const userApi = require('./api/userApi.js');
-const accountApi = require('./api/accountApi.js');
-//BOILER
+
 const express = require('express');
 const app = express();
 const methodOverride = require('method-override');
@@ -23,32 +21,6 @@ app.get('/users', (req, res) => {
     });
 });
 
-app.get('/users/:userId', (req, res) => {
-  //gets user
-  userApi.getUserById(req.params.userId)
-    .then(user => {
-      //gets accounts for user
-      accountApi.getAllAccountsByUserId(user._id)
-        .then(accounts => {
-          //put this inside of the inner most .then function :)
-          res.render('users/user', { user, accounts });
-        })
-    });
-});
-
-app.post('/users', (req, res) => {
-  userApi.createNewUser(req.body)
-    .then(() => {
-      res.render("users/created");
-   });
-});
-
-app.delete('/users/:userId', (req, res) => {
-  userApi.deleteUserById(req.params.userId)
-    .then(() => {
-      res.render("users/deleted");
-    });
-});
 
 //BOILER
 const PORT = 3000;
