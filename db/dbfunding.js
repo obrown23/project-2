11e2c0c1f36c80b9bdd909e210e3212f7326cf8f
KnightRider <omar.brown@comcast.net>
2019-04-30 12:03:53 -0400

    13th commit

--- db/dbfunding.js
+++ db/dbfunding.js
@@ -1,65 +1,47 @@
-let filmmakers = require('../api/filmmakers.js')
+let funding = require('../api/filmmakers.js')
 
-let newFilmmakers = [
+let newFunding = [
     {
-        film: "DrummerBoy",
-        director: "Axel Foley",
-        writer: "Don Cheatal",
-        // trailer: String,
+        name: "Bankside Films",
+        website: "https://www.bankside-films.com/home/",
+        email: "films@bankside-films.com",
+        phone: 4402076366085,
+    }, 
+    {
+        name: "Bertha BRITDOC Connect Fund",
+        website: "https://docsociety.org/funds/",
+        email: "bertha@films.org",
+        phone: 6067757070,
+    },
+    {
+        name: "DrummerBoy",
         website: "www.drummerboy.com",
         email: "drummerboy@drummerboy.org",
         phone: 6067757070,
     }, 
     {
-        film: "Dark Burner",
-        director: "Chaz Vex",
-        writer: "Duce Vannin",
-        // trailer: String,
-        website: "www.darkburner.com",
-        email: "darkburner2019@vice.org",
-        phone: 585434987,
-      
-    },
+        name: "DrummerBoy",
+        website: "www.drummerboy.com",
+        email: "drummerboy@drummerboy.org",
+        phone: 6067757070,
+    }, 
     {
-        film: "Sunset",
-        director: "Chaz Vex",
-        writer: "Duce Vannin",
-        // trailer: String,
-        website: "www.darkburner.com",
-        email: "darkburner2019@vice.org",
-        phone: 585434987,
-      
-    },
+        name: "DrummerBoy",
+        website: "www.drummerboy.com",
+        email: "drummerboy@drummerboy.org",
+        phone: 6067757070,
+    }, 
     {
-        film: "Ugly Dogs",
-        director: "Kelly Asbury",
-        writer: "David Horvath ",
-        // trailer: String,
-        website: "https://www.imdb.com/title/tt1946502/",
-        email: "darkburner2019@vice.org",
-        phone: 585434987,
-      
-    },
+        name: "DrummerBoy",
+        website: "www.drummerboy.com",
+        email: "drummerboy@drummerboy.org",
+        phone: 6067757070,
+    }, 
     {
-        film: "Captain Marvel",
-        director: "Anna Boden",
-        writer: "Ryan Fleck",
-        // trailer: String,
-        website: "https://www.imdb.com/title/tt4154664/",
-        email: "darkburner2019@vice.org",
-        phone: 585434987,
-      
-    },
-    {
-        film: "Little",
-        director: "Tina Gordon",
-        writer: "Tracy Oliver",
-        // trailer: String,
-        website: "https://www.imdb.com/title/tt8085790/",
-        email: "darkburner2019@vice.org",
-        phone: 585434987,
-      
-    }
-
+        name: "DrummerBoy",
+        website: "www.drummerboy.com",
+        email: "drummerboy@drummerboy.org",
+        phone: 6067757070,
+    },  
 ]
 filmmakers.FilmNew(newFilmmakers)
